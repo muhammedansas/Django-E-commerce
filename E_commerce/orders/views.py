9c90ac8d42d4f3514bb9c22dd25482174aa349ca
Muhammed Ansas pt <muhammedansas566@gmail.com>
2024-05-31 09:38:54 +0530

    updated

--- E_commerce/orders/views.py
+++ E_commerce/orders/views.py
@@ -102,7 +102,7 @@ def payments(request):
     )
     payment.save()
     order.payment = payment
-    
+    order.is_ordered = True
     order.save()
     return render(request,'cart_and_orders/payments.html')
         
