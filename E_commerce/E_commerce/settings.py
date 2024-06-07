b8a7570f60b638688a9faa2e9eb38267ffb8ec3f
Muhammed Ansas pt <muhammedansas566@gmail.com>
2024-06-07 20:08:52 +0530

    updated

--- E_commerce/E_commerce/settings.py
+++ E_commerce/E_commerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,10 +135,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = ''
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR/ 'static',
+
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR/ 'static',
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR = [
+os.path.join(BASE_DIR, 'static')
 ]
 
 
